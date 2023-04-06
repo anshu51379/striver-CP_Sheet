3f0dd76a0e4f678edc21301549d38fc93f5aa30a
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-04-06 16:54:21 +0530

    Update 1337A.cpp

---
+++ Maths/1337A.cpp
@@ -0,0 +1,15 @@
+// https://codeforces.com/problemset/problem/1337/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int tc;
+    cin>>tc;
+    int x, y, z;
+    while(tc--){
+        int a, b, c, d;
+        cin>>a>>b>>c>>d;
+        x=b; y=c; z=c; //idealCaseScenario
+        cout<<x<<" "<<y<<" "<<z<<endl;
+    }
+}
