33fee953e07337a80dc2c62d0e69a6bea7ce8bb1
Abhishek Kumar <80306592+anshu51379@users.noreply.github.com>
2023-04-06 17:44:12 +0530

    Create 959A.cpp

---
+++ Maths/959A.cpp
@@ -0,0 +1,13 @@
+// https://codeforces.com/problemset/problem/959/A
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n;
+    cin>>n;
+    if(n&1){
+        cout<<"Ehab";
+    }else{
+        cout<<"Mahmoud";
+    }
+}
